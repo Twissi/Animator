437a6296cbe94e81e62beb0cfbfc553816080d67
manuelhirsch <github@hirsch.it>
2012-11-27 20:19:26 +0100

    - removed unused import - changed animationListData to more specific type to satisfy compiler

--- src/main/java/org/hacklace/animator/gui/HomePanel.java
+++ src/main/java/org/hacklace/animator/gui/HomePanel.java
@@ -7,16 +7,15 @@ import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ListModel;
 
 public class HomePanel extends JPanel {
 	private static final long serialVersionUID = 1045750321890262891L;
 
 	JList<String> animationList;
-	DefaultListModel animationListData;
+	DefaultListModel<String> animationListData;
 
 	public HomePanel() {
-		animationListData = new DefaultListModel();
+		animationListData = new DefaultListModel<String>();
 		animationList = new JList<String>(animationListData);
 		JScrollPane animationListScrollPane = new JScrollPane(animationList);
 		// TODO: not working :( animationList.setMinimumSize(new Dimension(200, 50));
