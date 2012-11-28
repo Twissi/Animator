b9ebfe637ce3c5600f40b1ef5810fe9922602e9f
Monika Eggers <monika@lilly.(none)>
2012-11-28 17:21:39 +0100

    ReferenceDisplayBuffer delegate methods

--- src/main/java/org/hacklace/animator/displaybuffer/ReferenceDisplayBuffer.java
+++ src/main/java/org/hacklace/animator/displaybuffer/ReferenceDisplayBuffer.java
@@ -7,29 +7,30 @@ import org.hacklace.animator.enums.AnimationType;
 public class ReferenceDisplayBuffer extends DisplayBuffer {
 
 	private char letter;
+
 	private List<DisplayBuffer> list;
 
 	public ReferenceDisplayBuffer(char letter, List<DisplayBuffer> list) {
 		this.letter = letter;
 		this.list = list;
 	}
-	
+
 	private DisplayBuffer getReferencedDisplayBuffer() {
-	int index = letter - 'A';
-	DisplayBuffer otherDisplayBuffer = this.list.get(index);
-	return otherDisplayBuffer;
+		int index = letter - 'A';
+		DisplayBuffer otherDisplayBuffer = this.list.get(index);
+		return otherDisplayBuffer;
 	}
 
 	@Override
-	public int getStepWidth() { return
-		getReferencedDisplayBuffer().getStepWidth();
+	public int getStepWidth() {
+		return getReferencedDisplayBuffer().getStepWidth();
 	}
 
 	@Override
 	public AnimationType getAnimationType() {
 		return AnimationType.GRAPHIC;
 	}
-	
+
 	@Override
 	public boolean isReferenceBuffer() {
 		return true;
@@ -38,5 +39,36 @@ public class ReferenceDisplayBuffer extends DisplayBuffer {
 	public char getLetter() {
 		return this.letter;
 	}
-	
+	public void setLetter(char letter) {
+		this.letter = letter;
+	}
+	@Override
+	public boolean getValueAt(int x, int y) {
+		return getReferencedDisplayBuffer().data[x][y];
+	}
+	@Override
+	public void rewind() {
+		getReferencedDisplayBuffer().rewind();
+	}
+	@Override
+	public void moveLeft() {
+		getReferencedDisplayBuffer().moveLeft();
+	}
+	@Override
+	public void moveRight() {
+		getReferencedDisplayBuffer().moveRight();
+	}
+	@Override
+	public Grid getPrevious() {
+		return getReferencedDisplayBuffer().getPrevious();
+	}
+	@Override
+	public Grid getCurrent() {
+		return getReferencedDisplayBuffer().getCurrent();
+	}
+	@Override
+	public Grid getNext() {
+		return getReferencedDisplayBuffer().getNext();
+	}
+
 }
