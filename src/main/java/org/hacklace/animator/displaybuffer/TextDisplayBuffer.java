91ff4c333f8605aaf7f42b8d8901b3e54ad8b464
Manuel Hirsch <github@hirsch.it>
2012-12-22 17:20:21 +0100

    Closes: #11 "Special characters" TextDisplayBuffer is handling escapes now

--- src/main/java/org/hacklace/animator/displaybuffer/TextDisplayBuffer.java
+++ src/main/java/org/hacklace/animator/displaybuffer/TextDisplayBuffer.java
@@ -1,5 +1,7 @@
 package org.hacklace.animator.displaybuffer;
 
+import java.util.Arrays;
+
 import org.hacklace.animator.enums.AnimationType;
 
 public class TextDisplayBuffer extends DisplayBuffer {
@@ -19,7 +21,11 @@ public class TextDisplayBuffer extends DisplayBuffer {
 	public String getText() {
 		return text;
 	}
-
+	
+	private boolean isEscape(char c) {
+		return (c == '^' || c == '~' || c == '$');
+	}
+	
 	/**
 	 * side effect: update data (bits/bytes)
 	 * @param text
@@ -27,21 +33,52 @@ public class TextDisplayBuffer extends DisplayBuffer {
 	public void setText(String text) {
 		this.text = text;
 		int col = 0;
-		for (char c : text.toCharArray()) {
-			int[] animationBytes = FontUtil.getMinimumBytesForChar(c);
-			for (int i = 0; i < DisplayBuffer.COLUMNS; i++) {
+		for (int i = 0; i < text.length(); i++) {
+			int[] animationBytes;
+			char c = text.charAt(i);
+			if (isEscape(c)) {
+				// handle escape characters
+				i++;
+				// ignore escape character at the end of the string
+				if (i > text.length() - 1) return;
+				char next = text.charAt(i);
+				// escape for the character itself?
+				if (next == c) {
+					animationBytes = FontUtil.getMinimumBytesForChar(c);
+				} else {
+					switch (c) {
+					case '^':
+						animationBytes = FontUtil.getMinimumBytesForSpecial(next);
+						break;
+					default:
+						// TODO A reference to an animation in a text animation... What do?
+						// For a non-fatal fallback, display the escape sequence instead...
+						i--;
+						c = text.charAt(i);
+						animationBytes = FontUtil.getMinimumBytesForChar(c);
+					}
+					
+				}
+			} else {
+				// normal character
+				animationBytes = FontUtil.getMinimumBytesForChar(c);
+			}
+			for (int byteNum = 0; byteNum < DisplayBuffer.COLUMNS; byteNum++) {
 				for (int bit = 0; bit < 7; bit++) {
-					if (i < animationBytes.length) {
-						data[i + DisplayBuffer.COLUMNS * col][bit] = (animationBytes[i] & (int)Math.pow(2, bit)) != 0;
+					if (byteNum < animationBytes.length) {
+						try {
+							data[byteNum + DisplayBuffer.COLUMNS * col][bit] = (animationBytes[byteNum] & (int)Math.pow(2, bit)) != 0;
+						} catch (ArrayIndexOutOfBoundsException ex) {
+							System.err.println("Error creating text buffer. col=" + col + " i=" + i + " c=" + c + " byteNum=" + byteNum);
+						}
 					} else {
 						// clear all bits after the last column from the font
-						data[i + DisplayBuffer.COLUMNS * col][bit] = false;
+						data[byteNum + DisplayBuffer.COLUMNS * col][bit] = false;
 					}
 				}
 			}
 			col++;
 		}
-
 	}
 
 	@Override
