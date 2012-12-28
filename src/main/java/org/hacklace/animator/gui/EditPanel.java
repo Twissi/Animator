e252f74d739cf81a1c8afe04bc9a531c0f3301e0
Manuel Hirsch <github@hirsch.it>
2012-12-28 20:27:46 +0100

    Removed unused import

--- src/main/java/org/hacklace/animator/gui/EditPanel.java
+++ src/main/java/org/hacklace/animator/gui/EditPanel.java
@@ -17,7 +17,6 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
