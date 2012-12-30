b28706c3c1ecd69a14240fcb34cf7c1eb5ffaed5
Manuel Hirsch <github@hirsch.it>
2012-12-30 03:57:27 +0100

    Added note to ini file about how to address ports and one more unittest The unittest can be (ab)used to find the correct port but basically can't fail.

--- src/test/java/org/hacklace/animator/FlashTest.java
+++ src/test/java/org/hacklace/animator/FlashTest.java
@@ -1,10 +1,16 @@
 package org.hacklace.animator;
 
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.PortInUseException;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
 
 import junit.framework.TestCase;
 import junitx.framework.FileAssert;
@@ -44,6 +50,32 @@ public class FlashTest extends TestCase {
 				HacklaceConfigManager.HACKLACE_CHARSET));
 		flashExporter.writeTo(stream, output);
 		FileAssert.assertBinaryEquals(exampleFlash, output);
+	}
+	
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testEnumeratePorts() {
+        HashSet<CommPortIdentifier> h = new HashSet<CommPortIdentifier>();
+        Enumeration thePorts = CommPortIdentifier.getPortIdentifiers();
+        while (thePorts.hasMoreElements()) {
+            CommPortIdentifier com = (CommPortIdentifier) thePorts.nextElement();
+            switch (com.getPortType()) {
+            case CommPortIdentifier.PORT_SERIAL:
+                try {
+                    CommPort thePort = com.open("CommUtil", 50);
+                    thePort.close();
+                    h.add(com);
+                    System.out.println("Port "  + com.getName() + " is free.");
+                } catch (PortInUseException e) {
+                    System.out.println("Port "  + com.getName() + " is in use.");
+                } catch (Exception e) {
+                    System.err.println("Failed to open port " +  com.getName());
+                    e.printStackTrace();
+                }
+            }
+        }
+        // return h;
 	}
 
 }
