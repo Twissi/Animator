5daf4c72393028384ca907b981b7d624ccc8275d
Manuel Hirsch <github@hirsch.it>
2013-01-06 15:44:59 +0100

    [fixes #101] Predefined animation: Zu viele Frames

--- src/main/java/org/hacklace/animator/gui/EditReferencePanel.java
+++ src/main/java/org/hacklace/animator/gui/EditReferencePanel.java
@@ -1,14 +1,25 @@
 package org.hacklace.animator.gui;
 
+import org.hacklace.animator.IniConf;
 import org.hacklace.animator.displaybuffer.DisplayBuffer;
+import org.hacklace.animator.displaybuffer.ReferenceDisplayBuffer;
 
 public class EditReferencePanel extends EditPanel {
 
 	private static final long serialVersionUID = 1648909855223726429L;
-	
+
 	public EditReferencePanel(DisplayBuffer displayBuffer) {
 		super(displayBuffer);
 		ledPanel.setEnabled(false);
+	}
+
+	@Override
+	protected int getMaximumGrid() {
+		int columns = ((ReferenceDisplayBuffer) buffer).countUsedColumns();
+		int grids = ((int) (columns / GRID_COLS)) - NUM_GRIDS_TO_SHOW;
+		if (grids < 0)
+			grids = 0;
+		return grids;
 	}
 
 }
