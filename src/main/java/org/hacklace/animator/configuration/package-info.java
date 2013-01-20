4887fec2f3e62107f2fbd939c70411586893c328
Monika Eggers <monikakrug@arcor.de>
2013-01-20 17:41:12 +0100

    GPL

--- src/main/java/org/hacklace/animator/configuration/package-info.java
+++ src/main/java/org/hacklace/animator/configuration/package-info.java
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * This program is made available under the terms of the GPLv3 or higher
+ * which accompanies it and is available at http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
 /**
  * Package for classes relating to configuration lines in hacklace files or as raw data input from the user
  * @author monika
--- src/main/java/org/hacklace/animator/enums/ErrorType.java
+++ src/main/java/org/hacklace/animator/enums/ErrorType.java
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * This program is made available under the terms of the GPLv3 or higher
+ * which accompanies it and is available at http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
 package org.hacklace.animator.enums;
 
 public enum ErrorType {
