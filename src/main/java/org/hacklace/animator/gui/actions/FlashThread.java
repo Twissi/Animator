5e3d1246afac9f7f6ea9a06c0ca4ca9e559aecca
Manuel Hirsch <github@hirsch.it>
2013-01-20 18:57:53 +0100

    added license note to new class

--- src/main/java/org/hacklace/animator/gui/actions/FlashThread.java
+++ src/main/java/org/hacklace/animator/gui/actions/FlashThread.java
@@ -1,3 +1,8 @@
+/*******************************************************************************
+This program is made available under the terms of the GPLv3 or higher
+which accompanies it and is available at http://www.gnu.org/licenses/gpl.html
+******************************************************************************/
+
 package org.hacklace.animator.gui.actions;
 
 import java.awt.Cursor;
